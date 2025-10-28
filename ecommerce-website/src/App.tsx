e443c490d80898a71a3a6fe7f31dec63889de87a
Tictactoons <tonanubi@gmail.com>
2025-10-28 09:19:21 -0500

    nav

--- ecommerce-website/src/App.tsx
+++ ecommerce-website/src/App.tsx
@@ -1,14 +1,21 @@
 
+import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import './index.css'
+import Home from './pages/Home/Home'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Cart from './pages/Cart/Cart'
 
 function App() {
-  
-
   return (
-    <>
+    <div className='app'>
      <Navbar />
-    </>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/order' element={<PlaceOrder />} />
+     </Routes>
+    </div>
   )
 }
 
--- ecommerce-website/src/components/Navbar/Navbar.tsx
+++ ecommerce-website/src/components/Navbar/Navbar.tsx
@@ -1,33 +1,35 @@
-
-
-
-import { assets } from '../../assets/assets'
-import './Navbar.css'
-
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
 
 const Navbar = () => {
+
+    const [menu, setMenu] = useState('deals');
+
   return (
-    <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+    <div className="navbar">
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li onClick={()=>setMenu("home")} className={menu == "home" ? "active" : ""}>Home</li>
+        <li onClick={()=>setMenu("deals")} className={menu == "deals" ? "active" : ""}>Deals</li>
+        <li onClick={()=>setMenu("new-arrivals")} className={menu == "new-arrivals" ? "active" : ""}>New Arrivals</li>
+        <li onClick={()=>setMenu("packages")} className={menu == "packages" ? "active" : ""}>Packages</li>
       </ul>
 
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
-        <button>
-            sign in
-        </button>
+
+        <div className="btn">
+          <button className="sign-in">Sign in</button>
+          <button className="sign-up">Sign up</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
